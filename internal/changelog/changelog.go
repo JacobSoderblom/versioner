a64365f27b97a1ba046c38082073764432facd9f
Jacob Söderblom <jacob.soderblom@protonmail.com>
2023-10-19 22:12:53 +0200

    cmd for fetching changeset of latest tagged version

--- internal/changelog/changelog.go
+++ internal/changelog/changelog.go
@@ -7,8 +7,11 @@ import (
 	"strings"
 	"versioner/internal/config"
 	"versioner/internal/detect"
+	"versioner/internal/tag"
 
 	"github.com/docker/docker/pkg/namesgenerator"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/pkg/errors"
 )
 
@@ -68,6 +71,65 @@ func Add() error {
 	}
 
 	return nil
+}
+
+func GetLatestChangeSet(repo *git.Repository) (string, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	p := path.Join(wd, "CHANGELOG.md")
+	b, err := os.ReadFile(p)
+	if err != nil {
+		return "", err
+	}
+
+	_, _, ver, err := tag.FindLatest(repo, "")
+	if err != nil {
+		return "", err
+	}
+
+	tag := fmt.Sprintf("v%s", ver.String())
+
+	tags, err := repo.TagObjects()
+	if err != nil {
+		return "", err
+	}
+
+	foundCurrent := false
+	prevTag := ""
+	tags.ForEach(func(t *object.Tag) error {
+		if foundCurrent {
+			prevTag = t.Name
+			// just to stop the loop
+			return errors.New("found tag")
+		}
+
+		if t.Name == tag {
+			foundCurrent = true
+		}
+
+		return nil
+	})
+
+	c := string(b)
+
+	currTagTitle := fmt.Sprintf("## %s", tag)
+	prevTagTitle := fmt.Sprintf("## %s", prevTag)
+
+	currI := strings.Index(c, currTagTitle)
+	nextI := strings.Index(c, prevTagTitle)
+
+	if currI == -1 {
+		return "", nil
+	}
+
+	if nextI > len(c)-1 {
+		return "", nil
+	}
+
+	return c[currI:nextI], nil
 }
 
 func generateUniquePath() (string, error) {
